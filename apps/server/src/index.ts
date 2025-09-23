38ef79a9a41e2474bc0d9b80838684bda79c14cf
Fle-bihh <xilef.nahibel@gmail.com>
2025-09-23 16:30:43 +0100

    Ajout de la configuration Docker, mise à jour des scripts npm, intégration de dotenv pour la gestion des variables d'environnement, et création d'un fichier .env.example.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,2 +1,15 @@
 import "@matcha/shared";
+import express from "express";
+import { config } from "./config";
+
 console.log("SERVER app");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(config.port, () => {
+	console.log(`Example app listening on port ${config.port}`);
+});
