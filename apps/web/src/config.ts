73f66e94386691665c7cfa688867b2c08ed612bc
Fle-bihh <xilef.nahibel@gmail.com>
2025-10-16 10:42:13 +0100

    Réorganisation de la configuration des variables d'environnement et mise à jour de la logique de validation dans config.ts et vite.config.ts

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,15 +1,33 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 class Config {
 	public readonly port: number;
 	public readonly webUrl: string;
 	public readonly apiUrl: string;
 
 	constructor() {
-		this.port = Number(process.env.WEB_PORT) || 3001;
-		this.webUrl = process.env.WEB_URL || "http://localhost:3001";
-		this.apiUrl = process.env.API_URL || "http://localhost:3000";
+		const port = Number(import.meta.env.VITE_WEB_PORT);
+		const isValidPort = Number.isInteger(port) && port > 0 && port < 65536;
+
+		const webUrl = import.meta.env.VITE_WEB_URL;
+		const isValidWebUrl =
+			typeof webUrl === "string" && webUrl.startsWith("http");
+
+		const apiUrl = import.meta.env.VITE_SERVER_URL;
+		const isValidApiUrl =
+			typeof apiUrl === "string" && apiUrl.startsWith("http");
+
+		if (!isValidPort) {
+			throw new Error("Invalid VITE_WEB_PORT");
+		}
+		if (!isValidWebUrl) {
+			throw new Error("Invalid VITE_WEB_URL");
+		}
+		if (!isValidApiUrl) {
+			throw new Error("Invalid VITE_SERVER_URL");
+		}
+
+		this.port = port;
+		this.webUrl = webUrl;
+		this.apiUrl = `${apiUrl}/api/v1`;
 	}
 }
 
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,33 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { config } from "./src/config.ts";
+import dotenv from "dotenv";
+dotenv.config();
 
-if (process.env.NODE_ENV === "production") {
-	console.log("Production mode");
-} else {
-	console.log("Development mode");
-}
+export default defineConfig(({ mode }) => {
+	const host = "0.0.0.0";
+	const port = process.env.WEB_PORT ? parseInt(process.env.WEB_PORT) : 3001;
+	const allowedHosts = ["localhost", process.env.WEB_URL || ""];
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+	return {
+		plugins: [react()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
-	server: {
-		host: "0.0.0.0",
-		port: config.port,
-		allowedHosts: [config.webUrl],
-	},
-	optimizeDeps: {
-		include: ["@matcha/shared"],
-	},
-	build: {
-		commonjsOptions: {
-			include: [/packages/, /node_modules/],
+		server: {
+			host,
+			port,
+			allowedHosts,
 		},
-	},
+		optimizeDeps: {
+			include: ["@matcha/shared"],
+		},
+		build: {
+			commonjsOptions: {
+				include: [/packages/, /node_modules/],
+			},
+		},
+	};
 });
