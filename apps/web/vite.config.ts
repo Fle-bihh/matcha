fbb0fb4b26434ad1fe01d046088bb40a516cda66
Fle-bihh <xilef.nahibel@gmail.com>
2025-10-16 09:29:28 +0100

    Réorganisation de l'importation de dotenv dans la configuration de Vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-require("dotenv").config();
+// require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
