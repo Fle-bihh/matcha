86430ea63bdb722e3dea8b964a32184000433ac6
Fle-bihh <xilef.nahibel@gmail.com>
2025-12-15 17:27:32 +0000

    Install / configure MaterialUI library

--- apps/web/src/pages/entry.page.tsx
+++ apps/web/src/pages/entry.page.tsx
@@ -1,17 +1,20 @@
-
 import { ROUTES } from "@/constants";
+import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export function EntryPage() {
-	return (
-		<div>
-			<h1>Entry Page</h1>
-			<Link to={ROUTES.register}>
-				<button>Register</button>
-			</Link>
-			<Link to={ROUTES.login}>
-				<button>Login</button>
-			</Link>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Entry Page</h1>
+      <Link to={ROUTES.register}>
+        {/* <button>Register</button> */}
+        <Button variant="contained" color="primary">
+          Register
+        </Button>
+      </Link>
+      <Link to={ROUTES.login}>
+        <button>Login</button>
+      </Link>
+    </div>
+  );
 }
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,33 +5,38 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default defineConfig(({ mode }) => {
-	const host = "0.0.0.0";
-	const port = process.env.WEB_PORT ? parseInt(process.env.WEB_PORT) : 3001;
-	const allowedHosts = [process.env.WEB_URL || "localhost"];
+  const host = "0.0.0.0";
+  const port = process.env.WEB_PORT ? parseInt(process.env.WEB_PORT) : 3001;
+  const allowedHosts = [process.env.WEB_URL || "localhost"];
 
-	return {
-		plugins: [react()],
-		resolve: {
-			alias: {
-				"@": path.resolve(__dirname, "./src"),
-			},
-		},
-		server: {
-			host,
-			port,
-			allowedHosts,
-		},
-		preview: {
-			host,
-			port,
-		},
-		optimizeDeps: {
-			include: ["@matcha/shared"],
-		},
-		build: {
-			commonjsOptions: {
-				include: [/packages/, /node_modules/],
-			},
-		},
-	};
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      host,
+      port,
+      allowedHosts,
+    },
+    preview: {
+      host,
+      port,
+    },
+    optimizeDeps: {
+      include: [
+        "@matcha/shared",
+        "@mui/material",
+        "@emotion/react",
+        "@emotion/styled",
+      ],
+    },
+    build: {
+      commonjsOptions: {
+        include: [/packages/, /node_modules/],
+      },
+    },
+  };
 });
