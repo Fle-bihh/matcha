6ef974a901bd395a3260d5fdb512f9c7344b88a2
Fle-bihh <xilef.nahibel@gmail.com>
2025-12-21 11:16:47 +0000

    Update dependencies to React 19.2.3 and adjust CenteredPaper component props handling

--- apps/web/src/components/app/centered-paper.component.tsx
+++ apps/web/src/components/app/centered-paper.component.tsx
@@ -1,18 +1,19 @@
 import { Box, Paper } from "@mui/material";
-import { PropsWithChildren } from "react";
+import { ReactNode } from "react";
 import { PaperBackButton } from "../utils/paper-back-button.component";
 
 interface CenteredPaperProps {
   backButtonDisabled?: boolean;
   sx?: object;
+  children?: ReactNode;
 }
-export function CenteredPaper(props: PropsWithChildren & CenteredPaperProps) {
+export function CenteredPaper(props: CenteredPaperProps) {
   return (
     <Box>
       <Paper elevation={4} sx={{ borderRadius: 2, minWidth: "50vw", m: 4 }}>
         {!props.backButtonDisabled && <PaperBackButton />}
         <Box p={5} sx={props.sx}>
-          {props.children}
+          <>{props.children}</>
         </Box>
       </Paper>
     </Box>
