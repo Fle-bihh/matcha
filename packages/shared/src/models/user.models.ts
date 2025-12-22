3d590933bbd028c696e86b5da326c6074d15543a
Fle-bihh <xilef.nahibel@gmail.com>
2025-12-22 11:02:40 +0000

    Refactor user models to improve structure; add Gender and Orientation enums, and enhance User interface properties

--- packages/shared/src/models/user.models.ts
+++ packages/shared/src/models/user.models.ts
@@ -1,21 +1,44 @@
 import { BaseEntity } from "./base.models";
 
-export interface User extends BaseEntity {
-	username: string;
+export type UserLocation = any;
+
+export enum Gender {
+  Male = "male",
+  Female = "female",
+  Other = "other",
 }
 
-export interface AuthUser extends BaseEntity {
-	email: string;
-	username: string;
-	first_name: string;
-	last_name: string;
-	is_email_verified: boolean;
+export enum Orientation {
+  Heterosexual = "heterosexual",
+  Homosexual = "homosexual",
+  Bisexual = "bisexual",
+}
+
+export interface User extends BaseEntity {
+  username: string;
+  gender: Gender | null;
+  orientation: Orientation | null;
+  age: number | null;
+  bio: string | null;
+  pictures_urls: string[];
+  profile_picture_index: number | null;
+  interests: string[];
+  location: UserLocation | null;
+  fame_score: number;
+}
+
+export interface AuthUser extends User {
+  email: string;
+  first_name: string;
+  last_name: string;
+  is_email_verified: boolean;
+  is_profile_complete: boolean;
 }
 
 export interface AuthUserWithPassword extends AuthUser {
-	password: string; // hashed password
+  password: string; // hashed password
 }
 
 export type UserResult<T extends boolean> = T extends true
-	? AuthUserWithPassword | null
-	: AuthUser | null;
+  ? AuthUserWithPassword | null
+  : AuthUser | null;
