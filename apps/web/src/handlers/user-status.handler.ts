12bc7af8705709f3a07f0f1677356e1f143e8c71
Fle-bihh <xilef.nahibel@gmail.com>
2026-01-18 00:00:33 +0000

    feat: update UserStatusHandler to dispatch user status updates via patchEntity

--- apps/web/src/handlers/user-status.handler.ts
+++ apps/web/src/handlers/user-status.handler.ts
@@ -1,23 +1,31 @@
 import { BaseHandler } from "./base.handler";
 import { Socket } from "socket.io-client";
 import { EWebSocketEvents, UserStatusUpdateDto } from "@matcha/shared";
+import { patchEntity } from "@/store";
+import { EEntityTypes } from "@/types";
 
 export class UserStatusHandler extends BaseHandler {
 	public register(socket: Socket): void {
 		socket.on(
 			EWebSocketEvents.UserStatusUpdate,
-			this.handleUserStatusUpdate
+			this.handleUserStatusUpdate,
 		);
 	}
 
 	public unregister(socket: Socket): void {
 		socket.off(
 			EWebSocketEvents.UserStatusUpdate,
-			this.handleUserStatusUpdate
+			this.handleUserStatusUpdate,
 		);
 	}
 
 	private handleUserStatusUpdate = (dto: UserStatusUpdateDto): void => {
-		this.snackbar.success("User status updated: " + JSON.stringify(dto));
+		this.dispatch(
+			patchEntity({
+				entityType: EEntityTypes.Users,
+				id: dto.userId.toString(),
+				entity: { status: dto.status },
+			}),
+		);
 	};
 }
