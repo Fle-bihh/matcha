767b8ab0ad5f24d4b5a6ba11dd013f62af659437
Fle-bihh <xilef.nahibel@gmail.com>
2026-01-20 12:23:34 +0000

    feat: export base-message model in messages index for improved message handling

--- packages/shared/src/models/message.models.ts
+++ packages/shared/src/models/message.models.ts
@@ -1,3 +1,4 @@
+export * from "./messages";
 import { DeletedMessage, SystemMessage, TextMessage } from "./messages";
 
 export type Message = TextMessage | SystemMessage | DeletedMessage;
--- packages/shared/src/models/messages/index.ts
+++ packages/shared/src/models/messages/index.ts
@@ -1,2 +1,3 @@
+export * from "./base-message.models";
 export * from "./system-message.models";
 export * from "./user-message.models";
